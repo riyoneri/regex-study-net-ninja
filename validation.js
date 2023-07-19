481525fa4a0d6e8d6729f7662055baadd77e505a
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 11:30:14 +0200

    inputs and their event listeners

--- validation.js
+++ validation.js
@@ -1,1 +1,8 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (evt) => {
+    validate(evt.target, patterns[evt.target.name]);
+  });
+});
