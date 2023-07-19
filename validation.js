d00b52e5644cb844de4ad417b6044e1f5ad867c7
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 11:31:20 +0200

    telephone input regex

--- validation.js
+++ validation.js
@@ -1,6 +1,10 @@
 // validation script here
 const inputs = document.querySelectorAll("input");
 
+const patterns = {
+  telephone: /^\d{11}$/,
+};
+
 //Validation function
 function validate(field, regex) {
   field.className = regex.test(field.value) ? "valid" : "invalid";
