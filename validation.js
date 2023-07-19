e0749ac813eb0a777fe912daebe05c3c7f3f2c65
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 11:30:48 +0200

    add validate function

--- validation.js
+++ validation.js
@@ -1,6 +1,12 @@
 // validation script here
 const inputs = document.querySelectorAll("input");
 
+//Validation function
+function validate(field, regex) {
+  field.className = regex.test(field.value) ? "valid" : "invalid";
+}
+
+
 inputs.forEach((input) => {
   input.addEventListener("keyup", (evt) => {
     validate(evt.target, patterns[evt.target.name]);
